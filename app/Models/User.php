99b53b86af873b7efb3e75943379f44c9fdfedf2
Ed-sudo-collab <zerboedmond4@gmail.com>
2025-09-03 22:18:47 +0200

    Ajout socle technique Laravel + Vue + packages

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,73 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+// Ajouter Spatie Permission
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, HasProfilePhoto, Notifiable, TwoFactorAuthenticatable, HasRoles, SoftDeletes;
+
+    // Si tu veux utiliser le même nom de PK que ta table SQL
+    protected $primaryKey = 'id';
+    protected $keyType = 'int';
+    public $incrementing = true;
 
     /**
-     * The attributes that are mass assignable.
+     * Les attributs assignables en masse
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
+        'nom_utilisateur',
         'email',
         'password',
+        'type_utilisateur',
+        'statut',
+        'dernier_login',
+        'date_creation',
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * Les attributs cachés pour la sérialisation
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * Les attributs à ajouter automatiquement au tableau
      *
-     * @return array<string, string>
+     * @var array<int, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $appends = [
+        'profile_photo_url',
+    ];
+
+    /**
+     * Casts des attributs
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'dernier_login' => 'datetime',
+        'date_creation' => 'datetime',
+        'deleted_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,58 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Ici on définit toutes les routes web avec Inertia
+|
+*/
+
+// Page d'accueil
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+// Routes protégées par auth + jetstream
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+
+    // Dashboard
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    // Module Etudiants
+    Route::get('/etudiants', function () {
+        return Inertia::render('Etudiants/Index');
+    })->name('etudiants.index');
+
+    // Module Inscriptions
+    // Module Inscriptions
+    Route::get('/inscriptions', function () {
+        return Inertia::render('Inscriptions/Index');
+    })->name('inscriptions.index');
+
+    // Module Paiements
+    Route::get('/paiements', function () {
+        return Inertia::render('Paiements/Index');
+    })->name('paiements.index');
+
+
+
+});
+
+
